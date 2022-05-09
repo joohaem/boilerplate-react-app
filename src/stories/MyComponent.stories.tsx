aeb43371858b6fe3ca2c853a56c03fcda25f009f
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-09 21:01:38 +0900

    [feat] MyComponent 생성

---
+++ src/stories/MyComponent.stories.tsx
@@ -0,0 +1,9 @@
+import MyComponent from "./MyComponent";
+
+export default {
+  title: "Example/MyComponent",
+  component: MyComponent,
+};
+
+export const Basic = () => <MyComponent />;
+// export const WithProp = () => <MyComponent prop="value" />;
