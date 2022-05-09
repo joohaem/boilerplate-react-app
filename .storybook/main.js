9bc91efa1e9a00b370eacc3d98e5568f821a8ca7
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-09 18:16:21 +0900

    [init] 절대경로 TsconfigPathsPlugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,11 @@ module.exports = {
     // "storybook-addon-styled-component-theme/dist/preset",
     // "storybook-dark-mode",
   ],
+  // 추가::절대경로로 import를 하기 위해 플러그인 설정
+  webpackFinal: async (config) => {
+    config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    return config;
+  },
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
