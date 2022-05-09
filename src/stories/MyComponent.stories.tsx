8329598205f4c88b876705941cc4e95f2f7fd443
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-09 21:18:29 +0900

    [feat] add decorators to MyComponent

--- src/stories/MyComponent.stories.tsx
+++ src/stories/MyComponent.stories.tsx
@@ -3,6 +3,13 @@ import MyComponent from "./MyComponent";
 export default {
   title: "Example/MyComponent",
   component: MyComponent,
+  decorators: [
+    (MyComponent: () => JSX.Element) => (
+      <div style={{ margin: "3em" }}>
+        <MyComponent />
+      </div>
+    ),
+  ],
 };
 
 export const Basic = () => <MyComponent />;
